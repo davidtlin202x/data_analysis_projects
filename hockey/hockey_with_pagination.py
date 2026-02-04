775274b0b981075af57877edaddf82bd0e85e4df
davidtlin202x <david.t.lin2@gmail.com>
2026-02-04 14:31:40 +0800

    Implement pagination for data scraping
    Scrape data from multiple pages and store in DataFrame.

---
+++ hockey/hockey_with_pagination.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import time
+
+data_rows = []
+for p in range(1,25):
+    url = f"https://www.scrapethissite.com/pages/forms/?page_num={p}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(f"Now scraping page {p}.")
+    s = bs(response.text, 'html.parser')
+    if p == 1:
+        columns = [c.get_text(strip=True) for c in s.find_all('th')]
+    
+    
+    table = s.find('table', class_='table').prettify()
+    for tr in s.find_all('tr')[1::]:
+        row = [x.get_text(strip=True) for x in tr.find_all('td')]
+        data_rows.append(row)
+
+df = pd.DataFrame(data_rows, columns=columns)
+print(df.head())
+print(df.shape)
